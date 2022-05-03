249c2ec05148227b73207e68633397d339a9f076
JessyJP <49342341+JessyJP@users.noreply.github.com>
2022-05-03 03:40:13 +0300

    Reaper web remote control is added via android webview
    Signed-off-by: JessyJP <49342341+JessyJP@users.noreply.github.com>

--- app/src/main/java/com/example/reastreamreceiverandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/reastreamreceiverandroidapp/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.reastreamreceiverandroidapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initial loading of the web control page
+        val controlWebView: WebView = findViewById(R.id.webControlView)
+        val controlURLView: TextView = findViewById(R.id.webControlURLinputView)
+
+        controlWebView.webViewClient = WebViewClient()
+        controlWebView.settings.javaScriptEnabled = true
+        val url: String = controlURLView.text.toString()
+//        controlWebView.loadUrl("http://192.168.0.100:12121/");
+        // to avoid having to use the IPs internet permissions should be granted to be able to resolve the IP
+        // "<uses-permission android:name="android.permission.INTERNET"/>"
+        //
+        controlWebView.loadUrl(url)
+
+
     }
 }
